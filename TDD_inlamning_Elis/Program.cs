5845cd20af6855e73d21b3ec5d8debe572fd6895
br <72916549+elisbihl@users.noreply.github.com>
2021-11-18 13:42:16 +0100

    Add project files.

---
+++ TDD_inlamning_Elis/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TDD_inlamning_Elis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var socialmedia = new SocialMedia();
+        }
+    }
+}
---
+++ TDD_inlamning_Elis/SocialMedia.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_inlamning_Elis
+{
+    public class SocialMedia
+    {
+
+        public string Input { get; set; }
+
+        public string UserName { get; set; }
+
+        public void Check()
+        {
+            UserName = Console.ReadLine();
+
+            switch (Input)
+            {
+                case "/post":
+                    break;
+                case "/timeline":
+                    break;
+                case "/follow":
+                    break;
+                case "/wall":
+                    break;
+                case "/send_message":
+                    break;
+                case "/read_message":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Post()
+        {
+
+        }
+
+        public void Timeline()
+        {
+
+        }
+
+        public void Follow()
+        {
+
+        }
+
+        public void Wall()
+        {
+
+        }
+
+        public string SendMessage(string message)
+        {
+            return message;
+        }
+
+        public string ReadMessage()
+        {
+            return Input;
+        }
+
+        public bool Tag(string message)
+        {
+            return false;
+        }
+    }
+}
